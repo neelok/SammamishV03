fb7ed3c80f495161b77314caa8ee25b12297b3e5
Alec Sharma <44650669+neelok@users.noreply.github.com>
2020-04-22 15:51:50 -0700

    Added the functionality

--- sammaCS/plumber.R
+++ sammaCS/plumber.R
@@ -8,28 +8,19 @@
 #
 
 library(plumber)
+source("global.R")
+source("func.R")
 
-#* @apiTitle Plumber Example API
+#* @apiTitle Sammaish Crime Summary
 
-#* Echo back the input
-#* @param msg The message to echo
-#* @get /echo
-function(msg = "") {
-    list(msg = paste0("The message is: '", msg, "'"))
-}
 
-#* Plot a histogram
+
+#* Plot a Bar plot
+#* @param var
 #* @png
 #* @get /plot
-function() {
-    rand <- rnorm(100)
-    hist(rand)
+function(var) {
+  makePlot(var)
 }
 
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b) {
-    as.numeric(a) + as.numeric(b)
-}
+
